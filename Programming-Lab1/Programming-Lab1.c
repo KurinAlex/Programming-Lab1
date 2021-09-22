643b8507886694953cf99b43bb57ed82f816497d
Kurin, Alex <olexkurin@gmail.com>
2021-09-22 22:33:23 +0300

    Add code

---
+++ Programming-Lab1/Programming-Lab1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+#define a 2.8
+#define b 16.4
+#define c -5.4
+
+int main()
+{
+	double x = b * pow(tan(c), 2) - 1 / pow(sin(c / a), 2);
+	double y = a * exp(-sqrt(a)) * cos(b * c / a);
+
+	printf("a=%f b=%f c=%f x=%f y=%f", a, b, c, x, y);
+
+	getchar();
+}
