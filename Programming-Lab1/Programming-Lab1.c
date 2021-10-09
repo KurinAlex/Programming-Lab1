0664f7139fac21d37de09479456737c2cb6b47cc
Kurin, Alex <olexkurin@gmail.com>
2021-10-09 11:32:29 +0300

    lab1 add variables

--- Programming-Lab1/Programming-Lab1.c
+++ Programming-Lab1/Programming-Lab1.c
@@ -1,16 +1,38 @@
 ﻿#include <stdio.h>
+
+#define _USE_MATH_DEFINES
 #include <math.h>
 
-#define a 2.8
-#define b 16.4
-#define c -5.4
+void EnterDouble(double* num, const char* number_name)
+{
+	printf("Enter %s:", number_name);
+	scanf_s("%lf", num);
+}
 
 int main()
 {
+	double a;
+	printf("a must be bigger than 0\n");
+	do
+	{
+		EnterDouble(&a, "a");
+	} while (a <= 0.0);
+
+	double b;
+	EnterDouble(&b, "b");
+
+	double c;
+	printf("c / a must not be divisible by pi\n");
+	do
+	{
+		EnterDouble(&c, "c");
+	} while (fmod(c / a, M_PI) == 0);
+
 	double x = b * pow(tan(c), 2) - 1 / pow(sin(c / a), 2);
 	double y = a * exp(-sqrt(a)) * cos(b * c / a);
 
-	printf("a=%f b=%f c=%f x=%f y=%f", a, b, c, x, y);
+	printf("a=%lf b=%lf c=%lf x=%lf y=%lf", a, b, c, x, y);
 
-	getchar();
+	getch();
+	return 0;
 }
